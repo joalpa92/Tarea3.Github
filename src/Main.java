5e20e521d08fc71b2dbb59d3ef4c1aedaea5c59b
Jorge <joalpa92@gmail.com>
2024-05-06 17:57:22 +0200

    "Subimos el archivo a gitHub"

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n;
-        System.out.println("¿Que quieres hacer?\n1.Imprimir Hola mundo\n2.Salir del programa");
+        System.out.println("¿Que quieres hacer?" +
+                "\n1.Imprimir Hola mundo" +
+                "\n2.Imprime un numero aleatorio" +
+                "\n3.Salir del programa");
         n=scanner.nextInt();
         switch (n){
             case 1:
                 System.out.println("Hello world!");
                 break;
             case 2:
-                System.out.println("Goodbye!!");
+                System.out.println(Math.random()*3);
+                break;
+            case 3:
+                 System.out.println("Goodbye!!");
+            break;
         }
     }
 }
