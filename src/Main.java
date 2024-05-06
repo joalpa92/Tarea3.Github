317a916f5a211016e75e6b1218e1851f8e662f93
Jorge <joalpa92@gmail.com>
2024-05-06 17:44:10 +0200

    Añadimos un menu al programa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        int n;
+        System.out.println("¿Que quieres hacer?\n1.Imprimir Hola mundo\n2.Salir del programa");
+        n=scanner.nextInt();
+        switch (n){
+            case 1:
+                System.out.println("Hello world!");
+                break;
+            case 2:
+                System.out.println("Goodbye!!");
+        }
     }
 }
